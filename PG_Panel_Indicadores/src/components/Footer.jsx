30e6f31347507cd9a7acf036fce8565612c8e5bc
José Mariano Reyes <josemarianoreyesh02@gmail.com>
2024-08-15 09:46:55 -0600

    teoria de color aplicada

--- PG_Panel_Indicadores/src/components/Footer.jsx
+++ PG_Panel_Indicadores/src/components/Footer.jsx
@@ -4,9 +4,9 @@ import './Layout.css';
 
 const Footer = () => {
   return (
-    <footer className="bg-orange text-white text-center py-3">
+    <footer className="bg-orange text-black text-center py-3">
       <Container>
-        <p className='text-white mb-0 py-4'>&copy; 2024 AgroIntelligence. Todos los derechos reservados.</p>
+        <p className='text-black mb-0 py-4'>&copy; 2024 AgroIntelligence. Todos los derechos reservados.</p>
       </Container>
     </footer>
   );
