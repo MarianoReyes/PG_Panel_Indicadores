fe80014eea0d8c2dcb3e4a8c9cc20bac1dcc4ec8
José Mariano Reyes <josemarianoreyesh02@gmail.com>
2024-10-06 18:04:04 -0600

    mapa mas pro

--- PG_Panel_Indicadores/src/components/tch/TchMap.jsx
+++ PG_Panel_Indicadores/src/components/tch/TchMap.jsx
@@ -356,7 +356,7 @@ const TchMap = () => {
           >
             {selectedFeature && <CenterPolygon feature={selectedFeature} />}
             <TileLayer
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
               attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             />
             {geoData && minTCH !== null && maxTCH !== null && (
