06a88210222633484fe89747688d31662ae704b8
José Mariano Reyes <josemarianoreyesh02@gmail.com>
2024-10-23 17:42:23 -0600

    ultimos tests

--- PG_Panel_Indicadores/src/setupTests.js
+++ PG_Panel_Indicadores/src/setupTests.js
@@ -7,7 +7,7 @@ import { server } from './mocks/server';
 // Mock de URL.createObjectURL para Vitest
 globalThis.URL.createObjectURL = vi.fn(() => 'mocked-url');
 
-// Mock de URL.revokeObjectURL (si lo usas)
+// Mock de URL.revokeObjectURL 
 globalThis.URL.revokeObjectURL = vi.fn();
 
 // Configuración de MSW
