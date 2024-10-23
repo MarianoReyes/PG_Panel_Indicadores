c5bdf8daa76486bfbf856ca1dc0ad15676261b0f
José Mariano Reyes <josemarianoreyesh02@gmail.com>
2024-10-23 13:50:08 -0600

    tests casi finalizados

---
+++ PG_Panel_Indicadores/src/mocks/handlers.js
@@ -0,0 +1,33 @@
+// src/mocks/handlers.js
+import { rest } from 'msw';
+
+export const handlers = [
+  rest.get(import.meta.env.VITE_URL_PREDICCION_TCH, (req, res, ctx) => {
+    // Devuelve una respuesta mock de tu API
+    const mockData = {
+      type: 'FeatureCollection',
+      features: [
+        {
+          type: 'Feature',
+          properties: {
+            id: '123',
+            TCHPRED_6Meses: '50',
+          },
+          geometry: {
+            type: 'Polygon',
+            coordinates: [
+              [
+                [-90.785, 14.305],
+                [-90.780, 14.310],
+                [-90.775, 14.305],
+                [-90.785, 14.305],
+              ],
+            ],
+          },
+        },
+        // Puedes agregar más features mock si es necesario
+      ],
+    };
+    return res(ctx.status(200), ctx.json(mockData));
+  }),
+];
--- PG_Panel_Indicadores/vite.config.js
+++ PG_Panel_Indicadores/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   test: {
     environment: 'jsdom',
-    setupFiles: './vitest.setup.js',
+    setupFiles: './src/setupTests.js',
     globals: true,
   },
 })
